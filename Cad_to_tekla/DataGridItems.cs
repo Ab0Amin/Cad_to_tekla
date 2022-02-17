210c976fa5fc9051e0f4fd313f185e0d23a5ed25
amin <amin@LAPTOP-GQQANM6K>
2022-02-17 22:02:13 +0200

    deleting un nessary methodsstart with UI

---
+++ Cad_to_tekla/DataGridItems.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Cad_to_tekla
+{
+    internal class DataGridItems
+    {
+       public string Symbol { set; get; }
+        public string TeklaProfiles { set; get; }
+        public string select { set; get; }
+
+
+
+    }
+   
+}
